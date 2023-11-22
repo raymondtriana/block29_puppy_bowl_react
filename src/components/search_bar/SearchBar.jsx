b172b8d1de48014dc6eedcbf86583a1a2da6bb13
raymond triana <raymondtriana@gmail.com>
2023-11-22 17:09:23 -0500

    search CSS and state var

--- src/components/search_bar/SearchBar.jsx
+++ src/components/search_bar/SearchBar.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./SearchBar.css";
 /**
  * Responsible for filtering the player list
  */
@@ -7,9 +8,20 @@ const filterType = {
   Contains: 1, //will filter for names that include input string
 };
 export default function SearchBar() {
+  const [search,setSearch] = useState("")
+
+  useEffect(()=>{
+    console.log("SEARCHING FOR: "+search)
+  },[search])
+
   return (
-    <div>
-      <input type="text" name="" id="" />
+    <div id="search-div">
+      <div id="inner-search-div">
+        <label id="search-label" htmlFor="searchBar">
+          Search:
+          <input id="search-input" type="text" name="searchBar" onChange={(e)=>{setSearch(e.target.value)}}/>
+        </label>
+      </div>
     </div>
   );
 }
